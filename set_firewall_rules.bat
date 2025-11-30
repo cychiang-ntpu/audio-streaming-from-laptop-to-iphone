3dd28afde6e304957d087386ecb974713f35dd3a
unknown <cychiang@mail.ntpu.edu.tw>
2025-11-30 22:27:51 +0800

    Remove Windows batch files - Raspberry Pi branch only uses shell scripts

--- set_firewall_rules.bat
+++
@@ -1,7 +0,0 @@
-
-@echo off
-netsh advfirewall firewall add rule name="Icecast 8000" dir=in action=allow protocol=TCP localport=8000
-netsh advfirewall firewall add rule name="HTTP-WAV 8080" dir=in action=allow protocol=TCP localport=8080
-netsh advfirewall firewall add rule name="RTSP 8554"   dir=in action=allow protocol=TCP localport=8554
-echo 防火牆規則已新增（8000/8080/8554）。
-pause
